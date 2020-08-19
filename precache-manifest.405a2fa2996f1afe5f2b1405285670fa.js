597955705da9e37b45599db52ba99aab26c07d2a
Vaibhav <me170003058@iiti.ac.in>
2020-08-19 17:38:31 +0530

    Updates

---
+++ precache-manifest.405a2fa2996f1afe5f2b1405285670fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870acfb8150daed7f0dea0ccb01e156b",
+    "url": "/aeroclubiiti/index.html"
+  },
+  {
+    "revision": "449137a6ff84a1d10934",
+    "url": "/aeroclubiiti/static/css/main.f605e57c.chunk.css"
+  },
+  {
+    "revision": "a5c2099170b7f762d1d8",
+    "url": "/aeroclubiiti/static/js/2.f1174b28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aeroclubiiti/static/js/2.f1174b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449137a6ff84a1d10934",
+    "url": "/aeroclubiiti/static/js/main.f88e1374.chunk.js"
+  },
+  {
+    "revision": "067274361be8e91da78e",
+    "url": "/aeroclubiiti/static/js/runtime-main.424ffadc.js"
+  },
+  {
+    "revision": "d4b23d4f3c0353597a2322450517142f",
+    "url": "/aeroclubiiti/static/media/data.d4b23d4f.jpg"
+  }
+]);
