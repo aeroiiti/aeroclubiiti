5d01967bdd53ba43cf491c24875337a17146ae82
Sarisha Das <sarishadas.810@gmail.com>
2020-10-31 14:07:30 +0530

    post display added

--- src/context/posts/PostState.js
+++ src/context/posts/PostState.js
@@ -10,9 +10,10 @@ const PostState = (props) => {
       {
         team: ["Member1", "Member2"],
         _id: "5f8d94c127488063686fbb88",
-        name: "Post 1",
+        name: "Swarm of drones to locate leopard in campus",
         link: "abc.com",
-        description: "Lorem Ipsum",
+        description:
+          "We often hear about wildlife spotted in campus , the most dangerous of it is leopard. Due to grassy fields and such a big campus it is difficult and dangerous to trace the leopard in the campus .To encounter this we came up with this project of tracking the leopard with drones. Two or more drones that communicate with each other through swarm technology and track the movement of leopard on campus using image processing techniques and principles of ML and AI. Participants will also get to learn about working of drone as well.",
         coverURL: "temp.png",
         date: "2020-10-19T13:29:37.622Z",
         __v: 0,
@@ -20,9 +21,10 @@ const PostState = (props) => {
       {
         team: ["Member3", "Member4"],
         _id: "5f8d945f27488063686fbb87",
-        name: "Post 2",
+        name: "Drones for delivery of food from Zippy & Aladeen to hostel",
         link: "def.com",
-        description: "Lorem Ipsum Dolor Sit Amet",
+        description:
+          "By using the autonomous technology in drones we can deliver food & light weight stuff from one place to another. We will use GPS for tracking the path of destination & for drone autonomous we will use software like UGCS, Mission planner, QGroundControl It will make the delivery faster than normal.",
         coverURL: "temp2.png",
         date: "2020-10-19T13:27:59.123Z",
         __v: 0,
@@ -30,10 +32,11 @@ const PostState = (props) => {
       {
         team: ["Member1", "Member2"],
         _id: "5f8d919c19bf2aa272ceaac5",
-        name: "Post 1",
-        description: "Lorem Ipsum",
+        name: "Post 3",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         coverURL: "temp.png",
-        link: "abc.com",
+
         date: "2020-08-07T19:15:17.385Z",
       },
     ],
